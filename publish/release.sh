3137f473f6b87695ac1e65f394ea5c7bb4e04ed7
Tia <qiatia@outlook.com>
2025-04-01 11:28:52 +0800

    fix: 更新倒计时日期并优化发布脚本
    更新倒计时组件的目标日期至2025年10月1日，并简化发布脚本的构建流程，移除冗余的构建命令和调试输出

--- publish/release.sh
+++ publish/release.sh
@@ -3,22 +3,18 @@ servePath=/www/qiatia/dist
 #!/bin/bash
 execSCP(){
   node ./publish/setup.js
-  echo "==================== SyncFile ===================="
+  # echo "==================== SyncFile ===================="
 #  scp -r -P 922 ./dist/** root@122.112.177.219:$servePath
 #  服务器过期
   echo "==================== Done! ===================="
 }
 
 echo "==================== Build Code ===================="
-yarn build
-if [ $? -ne 0 ];then 
-npm run build
+
+npx vite build
 if [ $? -ne 0 ];then 
   echo "Build Fail！"
 else
-execSCP
-fi
-else
-  echo "====================yarn 打包完成===================="
+  echo "====================打包完成===================="
 execSCP
 fi
